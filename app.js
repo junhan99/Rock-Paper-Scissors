ad7c3c6b56b7a2ef678a64bb56f5e47b1c34a1b3
ChewJunHan <141676259+junhan99@users.noreply.github.com>
2023-10-11 02:21:06 +0800

    commit

--- app.js
+++ app.js
@@ -1,35 +1,65 @@
-function computerSelection() {
+function getComputerChoice() {
     let computerChoice = Math.floor((Math.random() * 3) + 1)
 
-    if (computerChoice === 1) {
-        computerChoice = 'Rock'
-    } else if (computerChoice === 2) {
-        computerChoice = 'Paper'
-    } else if (computerChoice === 3) {
-        computerChoice = 'Scissors'
-    }
+    // if (computerChoice === 1) {
+    //     computerChoice = 'Rock'
+    // } else if (computerChoice === 2) {
+    //     computerChoice = 'Paper'
+    // } else if (computerChoice === 3) {
+    //     computerChoice = 'Scissors'
+    // }
     
     return computerChoice
 }
 
 // console.log(getComputerChoice())
 
-function playerSelection() {
-    let playerChoice = prompt("Please make a selection.", "rock, paper, scissors".toLowerCase());
+function getPlayerChoice() {
+    let playerChoice = prompt("Please make a selection.", "rock, paper, scissors").toLowerCase();
     
-    let text;
     if (playerChoice == null || playerChoice == "") {
     alert("Please Try again, refresh the page.");
     } else {
-    return playerChoice
+        if (playerChoice === 'rock') {
+            return 1
+        } else if (playerChoice === 'paper'){
+            return 2
+        } else if (playerChoice === 'scissors'){
+            return 3
+        } else {
+            alert ('Please Try again, refresh the page.')
+        }     
+    }
+}
+
+
+function playRound(playerSelection, computerSelection) {
+    
+    if (playerSelection === 1 && computerSelection === 3){
+        return 'You win! Rock beats Scissors!'
+    } else if (computerSelection === 1 && PlayerSelection === 3){
+        return 'You Lose! Scissors loses to Rock!'
+    } else if (computerSelection > playerSelection) {
+        return 'You lose' 
+    } else if (playerSelection > computerSelection) {
+        return 'You win!'
+    } else if (computerSelection === playerSelection) {
+        return "It's a draw"
     }
 
 }
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
 
-function playRound(playerSelection, computerSelection) {
-    if 
-  }
-   
-  const playerChoice = playerSelection();
-  const computerChoice = computerSelection();
-  console.log(playRound(playerChoice, computerChoice));
+
+
+function game() {
+    getPlayerChoice()
+    playRound(playerSelection, computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+}
+
+game()
+game()
+game()
+
