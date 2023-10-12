f0c0fd2dee88bfdbed69f85fb48edaab7d583141
ChewJunHan <141676259+junhan99@users.noreply.github.com>
2023-10-13 02:59:57 +0800

    commit

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+computerScore = 0
+playerScore = 0
+roundWinner = ''
+
+
 function getComputerChoice() {
     let computerChoice = Math.floor((Math.random() * 3) + 1)
 
@@ -12,55 +17,76 @@ function getComputerChoice() {
     return computerChoice
 }
 
-// console.log(getComputerChoice())
+console.log(getComputerChoice())
 
 function getPlayerChoice() {
     let playerChoice = prompt("Please make a selection.", "rock, paper, scissors").toLowerCase();
-    
-    if (playerChoice == null || playerChoice == "") {
-    alert("Please Try again, refresh the page.");
-    } else {
-        if (playerChoice === 'rock') {
-            return 1
-        } else if (playerChoice === 'paper'){
-            return 2
-        } else if (playerChoice === 'scissors'){
-            return 3
-        } else {
-            alert ('Please Try again, refresh the page.')
-        }     
+    return playerChoice
+    // if (playerChoice == null || playerChoice == "") {
+    // alert("Please Try again, refresh the page.");
+    // } else {
+    //     if (playerChoice === 'rock') {
+    //         return 'rock'
+    //     } else if (playerChoice === 'paper'){
+    //         return 'paper'
+    //     } else if (playerChoice === 'scissors'){
+    //         return 'scissors'
+    //     } else {
+    //         alert ('Please Try again, refresh the page.')
+    //     }     
+    // }
+}
+console.log(getPlayerChoice());
+
+function updateScore() {
+    if (roundWinner = 'player') {
+        console.log(`You win! your current score is: ${playerScore}, computer score is: ${computerScore}`)
+    }
+
+    if (roundWinner = 'computer') {
+        console.log(`You lose! your current score is: ${playerScore}, computer score is: ${computerScore}`)
+    }
+
+    if (roundWinner = 'tie') {
+        console.log(`Tied! your current score is: ${playerScore}, computer score is: ${computerScore}`)
     }
 }
 
+const playerSelection = '';
+const computerSelection = getComputerChoice();
 
 function playRound(playerSelection, computerSelection) {
     
-    if (playerSelection === 1 && computerSelection === 3){
-        return 'You win! Rock beats Scissors!'
-    } else if (computerSelection === 1 && PlayerSelection === 3){
-        return 'You Lose! Scissors loses to Rock!'
-    } else if (computerSelection > playerSelection) {
-        return 'You lose' 
-    } else if (playerSelection > computerSelection) {
-        return 'You win!'
-    } else if (computerSelection === playerSelection) {
-        return "It's a draw"
+    if (playerSelection === computerSelection){
+        roundWinner = 'tie'
     }
-
+    if (
+    (computerSelection === 'rock' && playerSelection === 'scissors') ||
+    (computerSelection === 'paper' && playerSelection === 'rock') ||
+    (computerSelection === 'scissors' && playerSelection === 'paper')){
+        roundWinner = 'computer'
+        computerScore++
+    }
+    if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')){
+        roundWinner = 'player'
+        playerScore++
+    }
+    updateScore()
 }
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
+
+
 
 
 
 function game() {
-    getPlayerChoice()
+    getComputerChoice();
+    getPlayerChoice();
     playRound(playerSelection, computerSelection);
     console.log(playRound(playerSelection, computerSelection));
 }
-
-game()
-game()
 game()
 
 // variable for player Score 
@@ -74,3 +100,90 @@ game()
 // the if statement for each scenario when playing
 
 // basically do the same as above but using strings instead of ints
+
+
+
+function getComputerSelection() {
+    let computerChoice = Math.floor((Math.random() * 3) + 1)
+
+    if (computerChoice === 1) {
+        computerChoice = 'rock'
+    } else if (computerChoice === 2) {
+        computerChoice = 'paper'
+    } else if (computerChoice === 3) {
+        computerChoice = 'scissors'
+    }
+    
+    return computerChoice
+}
+
+function getPlayerChoice() {
+    const playerChoice = prompt("Please make a selection.", "rock, paper, scissors").toLowerCase();
+    return playerChoice
+}
+function playRound(playerSelection, computerSelection) {
+
+
+    if (playerSelection === computerSelection){
+        roundWinner = 'tie'
+    }
+    else if (
+    (computerSelection === 'rock' && playerSelection === 'scissors') ||
+    (computerSelection === 'paper' && playerSelection === 'rock') ||
+    (computerSelection === 'scissors' && playerSelection === 'paper')){
+        roundWinner = 'computer';
+    }
+    else if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')){
+        roundWinner = 'player';
+    }
+    updateScore(roundWinner, playerSelection, computerSelection)
+}
+
+function updateScore() {
+    let computerScore = 0
+    let playerScore = 0
+    let roundWinner = ''
+
+    if (roundWinner = 'player') {
+        playerScore++;
+        console.log(`You win! your current score is: ${playerScore}, computer score is: ${computerScore}`)
+    } else if (roundWinner = 'computer') {
+        computerScore++
+        console.log(`You lose! your current score is: ${playerScore}, computer score is: ${computerScore}`)
+    } else if (roundWinner = 'tie') {
+        console.log(`Tied! your current score is: ${playerScore}, computer score is: ${computerScore}`)
+    }
+// console.log(roundWinner)
+// console.log(computerScore)
+// console.log(playerScore)
+}
+
+
+// console.log(roundWinner)
+// console.log(computerScore)
+// console.log(playerScore)
+
+let computerSelection = getComputerSelection()
+let playerSelection = getPlayerChoice()
+
+console.log(computerSelection)
+console.log(playerSelection)
+
+
+
+
+function game() {
+    // getComputerSelection();
+    // getPlayerChoice();
+    playRound(playerSelection, computerSelection);
+    playRound(playerSelection, computerSelection);
+    // console.log(playRound(playerSelection, computerSelection));
+}
+game()
+
+// while (playerScore <= 5 || computerScore <= 5) {
+//     game()
+// }
---
+++ script.js
@@ -0,0 +1,5 @@
+// figure out how to update global variable from function
+// understand how function work more.. perhaps watch some vids
+// especially how the parameters work
+// shits hard dont give up :)
+// remember why ure doing this
